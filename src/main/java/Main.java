cf8ba2a7069ff03304d827434e7ad6d3fa735240
Seung Min <parksm1415@gmail.com>
2026-07-20 13:07:17 +0900

    1단계

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int a=calculator.add(2, 3);
+        int b=calculator.div(6, 4);
+        int c=calculator.multiple(2, 7);
+        int d=calculator.sub(7, 4);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+}
